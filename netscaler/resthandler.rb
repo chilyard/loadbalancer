b51eed796eb5371355c2c4d8549451fa05c3c7d1
Chuck Hilyard <chuck.hilyard@reachlocal.com>
2016-07-14 17:44:19 -0700

    parsing arguments

--- netscaler/resthandler.rb
+++ netscaler/resthandler.rb
@@ -15,20 +15,38 @@ class NSLBRestHandler
 
     attr_reader :dc 
 
-    # get everything setup
-    # 
-    # handle args
-    # verify credentials
+    # initialization
+    #
+    # what we'll do, here:
+    # check arguments passed
+    # verify credentials against the netscaler endpoint
+    #
+    # required args: 
+    # dc - caller must pass the datacenter {wh, lax, iad, nrt, syd, ams}
+    # env - caller must pass sdlc environment {dev, qa, stg, prod}
+    # country - caller must pass country {usa, can, aus, jpn, gbr, eur}
+    # service - caller must pass the service "nice name," i.e. "yjpconnector" 
+    #
+    # optional args:
+    # username - this may come from the calling source, like rundeck.  or we could prompt the user
+    # password - this may come from the calling source, like rundeck.  or we could prompt the user
+    #
     def initialize(*args)
       print "initializing NSLBRestHandler\n"
       @dc = args[0]
+      @env = args[1]
+      @country = args[2]
+      @service = args[3]
+      username = args[4]
+      password = args[5]
+
       @lb_host = "lb.#{dc}.reachlocal.com"     
-      load_credentials
       build_uri
+      load_credentials(username, password)
     end
 
-    # build a basic uri object.  update the path locally for
-    # any functions requiring a change
+    # build a basic uri object.  update the path local to the function for
+    # any functions that require it
     def build_uri
         print "building uri object..."
         @uri = URI::HTTP.build({
@@ -43,21 +61,49 @@ class NSLBRestHandler
 
 
     # if we don't have credentials supplied by the cli or rundeck, prompt the user
-    def load_credentials
-	  @username, @password = RLCredentials.loadbalancer("lb")
-      print "username: ", @username, "\n"
-      print "password: "
-      @password = STDIN.noecho(&:gets).chomp
-      print "\n"
+    def load_credentials(username, password)
+        if username.empty? || password.empty?
+            # unused feature, for now  
+	        @username, @password = RLCredentials.loadbalancer("lb")
+            print "username: ", @username, "\n"
+            print "password: "
+            @password = STDIN.noecho(&:gets).chomp
+            print "\n"
+
+        else
+            print "username and password already set\n"
+            print "u: ", username, "\n"
+            print "p: ", password, "\n"
+        end
+        # we'll want to test the credentials here by calling the rest_login
+        # function
     end
 
 
     # login to the LB
     def call_rest_login
-        print "login to LB\n" 
-        @uri.path = "/nitro/v1/config/lbvserver/"
-        @request = Net::HTTP::Get.new(@uri)
-        @request.basic_auth "#{@username}", "#{@password}"
+        print "checking credentials..." 
+        @uri.path = "/nitro/v1/config/login/"
+        @request = Net::HTTP::Post.new(@uri)
+        @request.add_field('Content-Type', 'application/vnd.com.citrix.netscaler.login+json')
+        @request.body = '{
+            "login": 
+                {
+                "username":"chuck.hilyard",
+                "password":"blahblah"
+                }
+            }'
+
+        Net::HTTP.start(@uri.host, @uri.port) { |http|
+            response = http.request(@request)
+                if response.code == "201"
+                    print "credential check success!\n"
+                else
+                    print "credential check fail!\n"
+                    print "code: ", response.code.to_i, "\n"
+                    print "body: ", response.body, "\n"
+                end
+        }
     end 
 
 
@@ -79,6 +125,8 @@ class NSLBRestHandler
     end
 
     # create LB objects
+    #
+    # arguments: 
     def call_rest_create(type="null")
         print "creating a #{type}..."
         @uri.path = "/nitro/v1/config/lbvserver/"
